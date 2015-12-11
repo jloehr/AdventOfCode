8e12fac6a75d7c3fd736a15a02175255553a7677
Julian Löhr <julian.loehr@outlook.com>
2015-12-11 03:17:40 +0100

    Add solution for Day 4 part one

---
+++ Day4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Input = "ckczppom";
+            MD5 Hash = MD5.Create();
+            UInt64 Counter = 0;
+            byte[] Result;
+
+            do
+            {
+                Counter++;
+                String HashingString = Input + Counter;
+                Result = Hash.ComputeHash(Encoding.ASCII.GetBytes(HashingString));
+
+            } while (!HasEnoughLeadingZeros(Result, 5));
+
+            System.Console.WriteLine("Number: {0}", Counter);
+            Console.Write("Press any key to continue . . .");
+            Console.ReadKey(true);
+        }
+
+        static bool HasEnoughLeadingZeros(byte[] Hash, uint RequiredZeros)
+        {
+            uint LeadingZeros = 0;
+
+            for (int i = 0; i < Hash.Length; i++)
+            {
+                if (Hash[i] != 0x00)
+                {
+                    if ((Hash[i] <= 0x0F))
+                    {
+                        LeadingZeros++;
+                        return (LeadingZeros >= RequiredZeros);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                LeadingZeros += 2;
+                if (LeadingZeros >= RequiredZeros)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
