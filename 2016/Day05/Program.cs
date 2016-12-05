b68e6c4ed7fdd8b4c27d6f30a948bd587e01c83e
Julian Löhr <julian.loehr@outlook.com>
2016-12-05 12:56:14 +0100

    Day 5 part two

--- 2016/Day05/Program.cs
+++ 2016/Day05/Program.cs
@@ -6,40 +6,83 @@ namespace Day05
 {
     class Program
     {
+        struct Cursor
+        {
+            public int Top;
+            public int Left;
+        }
+
         static void Main(string[] args)
         {
             MD5 Hash = MD5.Create();
             string Input = "ffykfhsq";
-            string Password = "";
+
+            int PartOneLength = 0;
+            string PartTwo = "00000000";
+            bool[] CharachterLocked = new bool[8];
+            int PartTwoRemainingBlanks = 8;
             UInt64 Index = 0;
 
-            while(Password.Length < 8)
+            Cursor PartOneCursor;
+            Cursor PartTwoCursor;
+
+            Console.Write("Part One: ");
+            PartOneCursor.Top = Console.CursorTop;
+            PartOneCursor.Left = Console.CursorLeft;
+            Console.WriteLine();
+            Console.Write("Part Two: ");
+            PartTwoCursor.Top = Console.CursorTop;
+            PartTwoCursor.Left = Console.CursorLeft;
+
+            while (PartTwoRemainingBlanks > 0)
             {
                 string HashString = Input + Index;
                 byte[] Result = Hash.ComputeHash(Encoding.ASCII.GetBytes(HashString));
 
-                Password += GetPassordCharacter(Result);
+                bool ValidHash = HasEnoughLeadingZeros(Result, 5);
+                bool DoFancyOutput = (Index % 100 == 0);
+
+                if (ValidHash || DoFancyOutput)
+                {
+                    int Sixth = (Result[2] & 0x0F);
+                    int Seventh = ((Result[3] & 0xF0) >> 4);
+                    string Character = Seventh.ToString("x");              
+
+                    if (PartOneLength < 8)
+                    {
+                        Console.SetCursorPosition(PartOneCursor.Left, PartOneCursor.Top);
+                        Console.Write(Sixth.ToString("x"));
+
+                        if(ValidHash)
+                        {
+                            PartOneCursor.Left++;
+                            PartOneLength++;
+                        }
+                    }
+
+                    if ((Sixth < PartTwo.Length) && (!CharachterLocked[Sixth]))
+                    {
+                        PartTwo = PartTwo.Substring(0, Sixth) + Character + PartTwo.Substring(Sixth + 1, 7 - Sixth);
+
+                        if (ValidHash)
+                        {
+                            CharachterLocked[Sixth] = true;
+                            PartTwoRemainingBlanks--;
+                        }
+
+                        Console.SetCursorPosition(PartTwoCursor.Left, PartTwoCursor.Top);
+                        Console.Write(PartTwo);
+                    }
+                }               
 
                 Index++;
             }
 
-            Console.WriteLine(Password);
+            Console.WriteLine();
             Console.Write("Press any key to continue . . .");
             Console.ReadKey(true);
         }
-
-        static string GetPassordCharacter(byte[] Hash)
-        {
-            if(HasEnoughLeadingZeros(Hash, 5))
-            {
-                return (Hash[2] & 0x0F).ToString("x");
-            }
-            else
-            {
-                return String.Empty;
-            }
-        }
-
+        
         static bool HasEnoughLeadingZeros(byte[] Hash, uint RequiredZeros)
         {
             for(int i = 0; i < Hash.Length; i++)
