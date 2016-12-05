5b252a9de0d3954899fa67f03f2aafcc05fb6d0b
Julian Löhr <julian.loehr@outlook.com>
2016-12-05 12:06:01 +0100

    Day 5 part one

---
+++ 2016/Day05/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Day05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MD5 Hash = MD5.Create();
+            string Input = "ffykfhsq";
+            string Password = "";
+            UInt64 Index = 0;
+
+            while(Password.Length < 8)
+            {
+                string HashString = Input + Index;
+                byte[] Result = Hash.ComputeHash(Encoding.ASCII.GetBytes(HashString));
+
+                Password += GetPassordCharacter(Result);
+
+                Index++;
+            }
+
+            Console.WriteLine(Password);
+            Console.Write("Press any key to continue . . .");
+            Console.ReadKey(true);
+        }
+
+        static string GetPassordCharacter(byte[] Hash)
+        {
+            if(HasEnoughLeadingZeros(Hash, 5))
+            {
+                return (Hash[2] & 0x0F).ToString("x");
+            }
+            else
+            {
+                return String.Empty;
+            }
+        }
+
+        static bool HasEnoughLeadingZeros(byte[] Hash, uint RequiredZeros)
+        {
+            for(int i = 0; i < Hash.Length; i++)
+            {
+                if(Hash[i] != 0x00)
+                {
+                    if(Hash[i] <= 0x0F)
+                    {
+                        return (((i * 2) + 1) >= RequiredZeros);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                if(((i + 1) * 2) >= RequiredZeros)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
