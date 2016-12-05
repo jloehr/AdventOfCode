c4fa80cdb6b2524024910e106c59d322e1c43273
Julian Löhr <julian.loehr@outlook.com>
2016-12-05 13:49:19 +0100

    Day 2 part two

--- 2016/Day02/Day02.cpp
+++ 2016/Day02/Day02.cpp
@@ -3,11 +3,24 @@
 
 #include "stdafx.h"
 
-typedef std::pair<unsigned, unsigned> Coordinate;
+typedef std::pair<signed, signed> Coordinate;
+
+void PartOne(const StringVector & Lines);
+void PartTwo(const StringVector & Lines);
 
 int main()
 {
 	StringVector Lines = GetFileLines("Input.txt");
+
+	PartOne(Lines);
+	PartTwo(Lines);
+
+	system("pause");
+}
+
+void PartOne(const StringVector & Lines)
+{
+	std::cout << "Part One: ";
 
 	for (const std::string & Line : Lines)
 	{
@@ -49,6 +62,65 @@ int main()
 	}
 
 	std::cout << std::endl;
-	system("pause");
 }
 
+bool IsValid(signed X, signed Y)
+{
+	if ((X < 0) || (X > 4) || (Y < 0) || (Y > 4))
+	{
+		return false;
+	}
+
+	signed dX = std::abs(X - 2);
+	signed dY = std::abs(Y - 2);
+
+	return ((dX + dY) <= 2);
+}
+
+void PartTwo(const StringVector & Lines)
+{
+	static const std::string Keypad("**1***234*56789*ABC***D**");
+
+	std::cout << "Part Two: ";
+	Coordinate Position(0, 2);
+
+	for (const std::string & Line : Lines)
+	{
+		for (const char & Instruction : Line)
+		{
+			switch (Instruction)
+			{
+			case 'U':
+				if (IsValid(Position.first, Position.second - 1))
+				{
+					Position.second--;
+				}
+				break;
+			case 'D':
+				if (IsValid(Position.first, Position.second + 1))
+				{
+					Position.second++;
+				}
+				break;
+			case 'L':
+				if (IsValid(Position.first - 1, Position.second))
+				{
+					Position.first--;
+				}
+				break;
+			case 'R':
+				if (IsValid(Position.first + 1, Position.second))
+				{
+					Position.first++;
+				}
+				break;
+			}
+		}
+
+
+		size_t Key = Position.second * 5 + Position.first;
+		std::cout <<  Keypad[Key];
+	}
+
+	std::cout << std::endl;
+}
