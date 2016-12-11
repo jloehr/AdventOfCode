24e89a618b244e2178b6aa8b11374acd551beeb4
Julian Löhr <julian.loehr@outlook.com>
2016-12-11 01:42:03 +0100

    Day 10 part two

--- 2016/Day10/Day10.cpp
+++ 2016/Day10/Day10.cpp
@@ -7,31 +7,74 @@ struct Bot;
 typedef uint16_t BotID;
 typedef std::shared_ptr<Bot> BotPointer;
 typedef std::map<BotID, BotPointer> BotMap;
+typedef std::pair<BotID, uint8_t> BotValuePair;
+typedef std::vector<BotValuePair> BotValueVector;
+typedef std::queue<BotPointer> BotQueue;
 
 struct Bot
 {
 	BotID Number;
-	uint8_t LowValue = 0;
-	uint8_t HighValue = 0;
+	std::queue<uint8_t> Values;
 	BotPointer LowBot;
 	BotPointer HighBot;
 
 	bool HasBothValues()
 	{
-		return (HighValue != 0);
+		return (Values.size() >= 2);
+	}
+
+	void PushValue(uint8_t Value)
+	{
+		Values.push(Value);
+	}
+
+	bool Run(BotQueue & CommandQueue)
+	{
+		constexpr uint8_t LowTarget = 17;
+		constexpr uint8_t HighTarget = 61;
+
+		if (!HasBothValues())
+			return false;
+
+		uint8_t LowValue = Values.front();
+		Values.pop();
+		uint8_t HighValue = Values.front();
+		Values.pop();
+
+		if (LowValue > HighValue)
+		{
+			std::swap(LowValue, HighValue);
+		}
+
+		LowBot->PushValue(LowValue);
+		HighBot->PushValue(HighValue);
+
+		CommandQueue.push(LowBot);
+		CommandQueue.push(HighBot);
+
+		return ((LowValue == LowTarget) && (HighValue == HighTarget));
 	}
 };
 
-BotMap CreateFactory(const StringVectorVector & Lines);
-BotID GetBotNumber(const BotMap & Bots, uint8_t Low, uint8_t Hight);
+BotID MakeOutputID(BotID Number);
+BotMap CreateFactory(const StringVectorVector & Lines, BotValueVector & Input);
+BotID Run(BotMap & Bots, BotValueVector & Input);
 
 int main()
 {
 	StringVectorVector Lines = GetFileLineParts("Input.txt");
 
-	BotMap Bots = CreateFactory(Lines);
+	BotValueVector Input;
+	BotMap Bots = CreateFactory(Lines, Input);
 
-	std::cout << "17-61 Bot: " << static_cast<uint32_t>(GetBotNumber(Bots, 17, 61)) << std::endl;
+	BotID PartOne = Run(Bots, Input);
+
+	BotPointer Output0 = Bots[MakeOutputID(0)];
+	BotPointer Output1 = Bots[MakeOutputID(1)];
+	BotPointer Output2 = Bots[MakeOutputID(2)];
+
+	std::cout << "17-61 Bot: " << static_cast<uint32_t>(PartOne) << std::endl;
+	std::cout << "Output 0 * 1 * 2: " << static_cast<uint32_t>(Output0->Values.front() * Output1->Values.front() * Output2->Values.front()) << std::endl;
 
 	system("pause");
 
@@ -40,7 +83,7 @@ int main()
 
 BotID MakeOutputID(BotID Number)
 {
-	return (0x0100 & Number);
+	return (0x0100 + Number);
 }
 
 BotPointer GetBot(BotMap & Bots, BotID BotNumber)
@@ -56,54 +99,15 @@ BotPointer GetBot(BotMap & Bots, BotID BotNumber)
 	return Bot;
 }
 
-void GiveBotValue(BotPointer Bot, uint8_t Value)
-{
-	if (Bot->HasBothValues())
-	{
-		return;
-	}
-
-	if (Bot->LowValue == 0)
-	{
-		Bot->LowValue = Value;
-		return;
-	}
-
-	if (Bot->LowValue > Value)
-	{
-		std::swap(Bot->LowValue, Value);
-	}
-
-	Bot->HighValue = Value;
-
-
-	if(Bot->LowBot)
-		GiveBotValue(Bot->LowBot, Bot->LowValue);
-	if(Bot->HighBot)
-		GiveBotValue(Bot->HighBot, Bot->HighValue);
-}
-
-void GiveBotValue(BotMap & Bots, BotID BotNumber, uint8_t Value)
-{
-	GiveBotValue(GetBot(Bots, BotNumber), Value);
-}
-
-
-void FillBot(BotMap & Bots, BotID BotNumber, BotID LowBotNumber, BotID HighBotNumber)
+void ConnectBots(BotMap & Bots, BotID BotNumber, BotID LowBotNumber, BotID HighBotNumber)
 {
 	BotPointer Bot = GetBot(Bots, BotNumber);
 
 	Bot->LowBot = GetBot(Bots, LowBotNumber);
 	Bot->HighBot = GetBot(Bots, HighBotNumber);
-
-	if (Bot->HasBothValues())
-	{
-		GiveBotValue(Bot->LowBot, Bot->LowValue);
-		GiveBotValue(Bot->HighBot, Bot->HighValue);
-	}
 }
 
-BotMap CreateFactory(const StringVectorVector & Lines)
+BotMap CreateFactory(const StringVectorVector & Lines, BotValueVector & Input)
 {
 	BotMap Bots;
 
@@ -114,7 +118,7 @@ BotMap CreateFactory(const StringVectorVector & Lines)
 			uint8_t Value = static_cast<uint8_t>(std::stoul(Line[1]));
 			BotID BotNumber = static_cast<BotID>(std::stoul(Line[5]));
 
-			GiveBotValue(Bots, BotNumber, Value);
+			Input.emplace_back(BotNumber, Value);
 		}
 		else
 		{
@@ -126,24 +130,41 @@ BotMap CreateFactory(const StringVectorVector & Lines)
 				LowBotNumber = MakeOutputID(LowBotNumber);
 
 			if (Line[10] == "output")
-				LowBotNumber = MakeOutputID(LowBotNumber);
+				HighBotNumber = MakeOutputID(HighBotNumber);
 
-			FillBot(Bots, BotNumber, LowBotNumber, HighBotNumber);
+			ConnectBots(Bots, BotNumber, LowBotNumber, HighBotNumber);
 		}
 	}
 
 	return Bots;
 }
 
-BotID GetBotNumber(const BotMap & Bots, uint8_t Low, uint8_t Hight)
+BotID Run(BotMap & Bots, BotValueVector & Input)
 {
-	for (const auto & Bot : Bots)
+	BotID PartOne = 0;
+	BotQueue RunQueue;
+
+	for (const BotValuePair & ValueInput : Input)
 	{
-		if ((Bot.second->LowValue == Low) && (Bot.second->HighValue == Hight))
+		BotPointer Bot = GetBot(Bots, ValueInput.first);
+		Bot->PushValue(ValueInput.second);
+
+		if (Bot->Run(RunQueue) && (PartOne == 0))
 		{
-			return Bot.first;
+			PartOne = Bot->Number;
 		}
 	}
 
-	return 0;
+	while (!RunQueue.empty())
+	{
+		BotPointer Bot = RunQueue.front();
+		RunQueue.pop();
+
+		if (Bot->Run(RunQueue) && (PartOne == 0))
+		{
+			PartOne = Bot->Number;
+		}
+	}
+
+	return PartOne;
 }
