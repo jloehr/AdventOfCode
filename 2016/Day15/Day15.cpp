f666417a90f08bd968cd03dfb673d49125288ef9
Julian Löhr <julian.loehr@outlook.com>
2016-12-15 06:24:16 +0100

    Day 15 part one

---
+++ 2016/Day15/Day15.cpp
@@ -0,0 +1,75 @@
+// Day15.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+struct Disc
+{
+	size_t Positions;
+	size_t Position;
+
+	Disc(size_t Positions, size_t Position)
+		:Positions(Positions), Position(Position)
+	{
+
+	}
+};
+
+typedef std::vector<Disc> DiscVector;
+
+DiscVector CreateDiscStack(const StringVectorVector & Lines);
+size_t GetButtonTime(DiscVector Discs);
+
+int main()
+{
+	StringVectorVector Lines = GetFileLineParts("Input.txt");
+
+	DiscVector Discs = CreateDiscStack(Lines);
+
+	std::cout << GetButtonTime(Discs) << std::endl;
+
+
+	system("pause");
+
+    return 0;
+}
+
+DiscVector CreateDiscStack(const StringVectorVector & Lines)
+{
+	DiscVector Discs;
+
+	for (StringVector Line : Lines)
+	{
+		size_t Positions = std::stoi(Line[3]);
+		size_t Start = std::stoi(Line[11]);
+
+		Discs.emplace_back(Positions, Start);
+	}
+
+	return Discs;
+}
+
+size_t GetButtonTime(DiscVector Discs)
+{
+	size_t Time = 0;
+	size_t DiscIndex = 0;
+
+	while (true)
+	{
+		bool GoesThrough = true;
+
+		for (size_t i = 0; i < Discs.size(); i++)
+		{
+			if (!(((Time + i + 1 + Discs[i].Positions) + Discs[i].Position) % Discs[i].Positions) == 0)
+			{
+				GoesThrough = false;
+				break;
+			}
+		}
+
+		if (GoesThrough)
+			return Time;
+
+		Time++;
+	}
+}
