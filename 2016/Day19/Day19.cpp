b32e831cbfc0ecc4354a340b3bb6224dc8a8debb
Julian Löhr <julian.loehr@outlook.com>
2016-12-20 12:33:17 +0100

    Change part one to constant computation similar to part two; Fix typo in part two

--- 2016/Day19/Day19.cpp
+++ 2016/Day19/Day19.cpp
@@ -12,6 +12,7 @@ int main()
 {
 	std::cout << "Part One Elf: " << PartOne(Input) << std::endl;
 	std::cout << "Part Two Elf: " << PartTwo(Input) << std::endl;
+
 	system("pause");
 
     return 0;
@@ -19,37 +20,24 @@ int main()
 
 uint64_t PartOne(uint64_t ElveCount)
 {
-	uint64_t ElfIndex = 0;
-	uint64_t Offset = 1;
+	double ElfLog = std::log2(ElveCount);
+	uint64_t ElfLowerBound = static_cast<uint64_t>(std::pow(2.0, std::floor(ElfLog)));
 
-	while (ElveCount != 1)
-	{
-		bool Even = (ElveCount % 2) == 0;
-		ElveCount /= 2;
-		Offset *= 2;
-
-		if (!Even)
-		{
-			ElfIndex += Offset;
-		}
-	}
-
-	return ElfIndex + 1;
+	return (((ElveCount - ElfLowerBound) * 2) + 1);
 }
 
 uint64_t PartTwo(uint64_t ElveCount)
 {
-	double ElveLog = std::log2(ElveCount) / std::log2(3);
-	uint64_t ElveLowerBound = static_cast<uint64_t>(std::pow(3.0, std::floor(ElveLog)));
+	double ElfLog = std::log2(ElveCount) / std::log2(3);
+	uint64_t ElfLowerBound = static_cast<uint64_t>(std::pow(3.0, std::floor(ElfLog)));
 
-	ElveCount -= ElveLowerBound;
-	if (ElveCount < ElveLowerBound)
+	ElveCount -= ElfLowerBound;
+	if (ElveCount < ElfLowerBound)
 	{
 		return ElveCount;
 	}
 	else
 	{
-		return ((ElveCount - ElveLowerBound) * 2) + ElveLowerBound;
+		return ((ElveCount - ElfLowerBound) * 2) + ElfLowerBound;
 	}
 }
-
