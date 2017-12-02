091478dbc077d2714b2f0a9f6ac78a8a1b2e5422
Julian Löhr <julian.loehr@outlook.com>
2017-12-02 13:27:51 +0100

    Day 02 part two

--- 2017/Day02/Day02.cpp
+++ 2017/Day02/Day02.cpp
@@ -7,24 +7,35 @@
 int main()
 {
 	StringVectorVector FileParts = GetFileLineParts("Input.txt");
-	uint64_t Checksum = 0;
+	uint64_t ChecksumPartOne = 0;
+	uint64_t ChecksumPartTwo = 0;
 
 	for (const auto & Line : FileParts)
 	{
-		uint64_t Max = 0;
-		uint64_t Min = UINT64_MAX;
+		std::vector<uint64_t> Numbers(Line.size());
+		std::transform(Line.begin(), Line.end(), Numbers.begin(), [](const std::string & Number) -> uint64_t {
+			return std::stoull(Number);
+		});
 
-		for (const auto & NumberString : Line)
+		std::sort(Numbers.begin(), Numbers.end());
+		ChecksumPartOne += *Numbers.rbegin() - *Numbers.begin();
+
+		for (auto NumberIt = Numbers.begin(); NumberIt != Numbers.end(); ++NumberIt)
 		{
-			uint64_t Number = std::stoull(NumberString);
-			Max = std::max(Max, Number);
-			Min = std::min(Min, Number);
+			for (auto DivisorIt = Numbers.begin(); DivisorIt != NumberIt; ++DivisorIt)
+			{
+				if ((*NumberIt % *DivisorIt) == 0)
+				{
+					ChecksumPartTwo += (*NumberIt / *DivisorIt);
+					NumberIt = Numbers.end() - 1;
+					break;
+				}
+			}
 		}
-
-		Checksum += Max - Min;
 	}
 
-	std::cout << "Checksum: " << Checksum << std::endl;
+	std::cout << "Checksum part one: " << ChecksumPartOne << std::endl;
+	std::cout << "Checksum part two: " << ChecksumPartTwo << std::endl;
 	system("pause");
 
     return 0;
