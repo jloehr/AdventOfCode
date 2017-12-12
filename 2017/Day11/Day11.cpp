75b3aae101a3380c3f6786e3bf9f024f3f1a9904
Julian Löhr <julian.loehr@outlook.com>
2017-12-12 12:41:04 +0100

    Day 11 part two

--- 2017/Day11/Day11.cpp
+++ 2017/Day11/Day11.cpp
@@ -3,11 +3,18 @@
 
 #include "stdafx.h"
 
+int StepsAway(const std::pair<int, int> & Position)
+{
+	return ((std::abs(Position.first) + std::abs(Position.second)) / 2);
+}
+
 int main()
 {
 	const StringVector Lines = GetFileLines("Input.txt");
 	const std::string & Input = Lines[0];
 	auto Position = std::make_pair( 0, 0 );
+	int MaxSteps = 0;
+	
 	
 	for (auto It = std::begin(Input); It != std::end(Input);)
 	{
@@ -25,9 +32,12 @@ int main()
 
 		Position.first += X;
 		Position.second += Y;
+
+		MaxSteps = std::max(MaxSteps, StepsAway(Position));
 	}
 
-	std::cout << "Steps: " << ((std::abs(Position.first) + std::abs(Position.second)) / 2 ) << std::endl;
+	std::cout << "Steps: " << StepsAway(Position) << std::endl;
+	std::cout << "Max steps away: " << MaxSteps << std::endl;
 
 	system("pause");
     return 0;
