d8b150f7c3873c6b4cc7192d2e969744c452f09b
Julian Löhr <julian.loehr@outlook.com>
2017-12-13 12:56:12 +0100

    Day 13 part two

--- 2017/Day13/Day13.cpp
+++ 2017/Day13/Day13.cpp
@@ -7,6 +7,7 @@
 int main()
 {
 	const StringVectorVector Lines = GetFileLineParts("Input.txt");
+	std::vector<std::pair<size_t, size_t>> Layers;
 	size_t Severity = 0;
 
 	for (const StringVector & Line : Lines)
@@ -15,9 +16,19 @@ int main()
 		size_t Depth = std::stoull(Line[1]);
 		if (TimeStep % ((Depth - 1) * 2) == 0)
 			Severity += TimeStep * Depth;
+
+		Layers.emplace_back(TimeStep, Depth);
 	}
 
 	std::cout << "Severity: " << Severity << std::endl;
+
+	size_t Delay = 0;
+	while (std::any_of(std::begin(Layers), std::end(Layers), [=](const auto & Layer)->bool { return (((Layer.first + Delay) % ((Layer.second - 1) * 2)) == 0); }))
+	{
+		++Delay;
+	}
+
+	std::cout << "Delay: " << Delay << std::endl;
 
 	system("pause");
     return 0;
