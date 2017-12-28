5901c18a6c2d595a7c7795bf4622f7394ac6af6c
Julian Löhr <julian.loehr@outlook.com>
2017-12-28 18:08:28 +0100

    Day 22 part two

--- 2017/Day22/Day22.cpp
+++ 2017/Day22/Day22.cpp
@@ -3,51 +3,79 @@
 
 #include "stdafx.h"
 
+constexpr size_t PartOneIterations = 10000;
+constexpr size_t PartTwoIterations = 10000000;
+
 using Position = std::pair<int64_t, int64_t>;
+enum class Cell { Clean, Weakened, Infected, Flagged };
+
+size_t Simulate(std::map<Position, Cell> Map, bool MultiState, size_t Iterations)
+{
+	Position Pos = { 0, 0 };
+	Position Direction{ 0, -1 };
+	uint64_t InfectionBurst = 0;
+
+	for (auto Burst = 0; Burst < Iterations; ++Burst)
+	{
+		auto Result = Map.find(Pos);
+		if (Result == std::end(Map))
+		{
+			Result = Map.insert(std::make_pair(Pos, Cell::Clean)).first;
+		}
+
+		switch (Result->second)
+		{
+		case Cell::Clean:
+			// turn left
+			std::swap(Direction.first, Direction.second);
+			Direction.second *= -1;
+			Result->second = MultiState ? Cell::Weakened : Cell::Infected;
+			break;
+		case Cell::Weakened: 
+			Result->second = Cell::Infected;
+			break;
+		case Cell::Infected:
+			// turn right
+			std::swap(Direction.first, Direction.second);
+			Direction.first *= -1;
+			Result->second = MultiState ? Cell::Flagged : Cell::Clean;
+			break;
+		case Cell::Flagged:
+			// reverse
+			Direction.first *= -1;
+			Direction.second *= -1;
+			Result->second = Cell::Clean;
+			break;
+		}
+
+		InfectionBurst += Result->second == Cell::Infected ? 1 : 0;
+
+		Pos.first += Direction.first;
+		Pos.second += Direction.second;
+	}
+
+	return InfectionBurst;
+}
 
 int main()
 {
 	const StringVector Lines = GetFileLines("Input.txt");
-	std::set<Position> Infected;
+	std::map<Position, Cell> Map;
 	Position Pos{ 0 , -1 * (Lines.size()/2) };
-	uint64_t InfectionBurst = 0;
 
 	for (const auto & Line : Lines)
 	{
 		Pos.first = -1 * (Line.size() / 2);
 		for (char Cell : Line)
 		{
-			if (Cell == '#')
-				Infected.insert(Pos);
+			Map.insert(std::make_pair(Pos, (Cell == '#') ? Cell::Infected : Cell::Clean));
 			Pos.first++;
 		}
 		Pos.second++;
 	}
 
-	Pos = { 0, 0 };
-	Position Direction{ 0, -1 };
-	for (auto Burst = 0; Burst < 10000; ++Burst)
-	{
-		auto Result = Infected.find(Pos);
-		if (Result == std::end(Infected))
-		{
-			InfectionBurst++;
-			Infected.insert(Pos);
-			std::swap(Direction.first, Direction.second);
-			Direction.second *= -1;
-		}
-		else
-		{
-			Infected.erase(Result);
-			std::swap(Direction.first, Direction.second);
-			Direction.first *= -1;
-		}
-
-		Pos.first += Direction.first;
-		Pos.second += Direction.second;
-	}
-
-	std::cout << "Burst of Infection: " << InfectionBurst << std::endl;
+	std::cout << "Burst of Infection part one: " << Simulate(Map, false, PartOneIterations) << std::endl;
+	std::cout << "Burst of Infection part two: " << Simulate(Map, true, PartTwoIterations) << std::endl;
 
 	system("pause");
     return 0;
--- 2017/Day22/stdafx.h
+++ 2017/Day22/stdafx.h
@@ -10,7 +10,7 @@
 #include <stdio.h>
 #include <tchar.h>
 
-#include <set>
+#include <map>
 
 #include "../../Common/Common.h"
 
