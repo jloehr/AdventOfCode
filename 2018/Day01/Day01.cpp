d4f4388cd4753b07b54d5e10b36b106b6a404dda
Julian Löhr <julian.loehr@outlook.com>
2018-12-01 17:33:13 +0100

    Day 01 part two

--- 2018/Day01/Day01.cpp
+++ 2018/Day01/Day01.cpp
@@ -6,8 +6,19 @@
 int main()
 {
 	int64_t Frequency = 0;
-	StringVector File = GetFileLines("Input.txt");
+	std::set<int64_t> Frequencies;
+	std::vector<int64_t> File = GetValues<int64_t>("Input.txt");
+	std::vector<int64_t>::iterator Line = File.begin();
 
-	std::for_each(File.begin(), File.end(), [&](const std::string & Line) { Frequency += std::stoll(Line); });
-	std::cout << "Part One: " << Frequency << std::endl;
+	std::cout << "Part One: " << std::accumulate<std::vector<int64_t>::iterator, int64_t>(File.begin(), File.end(), 0) << std::endl;
+
+	while (Frequencies.insert(Frequency).second)
+	{
+		Frequency += *Line;
+
+		if (++Line == File.end())
+			Line = File.begin();
+	}
+
+	std::cout << "Part Two: " << Frequency << std::endl;
 }
