810a233819eb54ecb61ecda7192754f8c3f7047d
Julian Löhr <julian.loehr@outlook.com>
2018-12-02 16:26:52 +0100

    Day 02 part two

--- 2018/Day02/Day02.cpp
+++ 2018/Day02/Day02.cpp
@@ -2,23 +2,33 @@
 //
 
 #include "pch.h"
-#include <iostream>
+
+unsigned int PartOne(const StringVector & Lines);
+std::string PartTwo(const StringVector & Lines);
 
 int main()
 {
 	StringVector File = GetFileLines("Input.txt");
+	
+	std::cout << "Part One: " << PartOne(File) << std::endl;
+	std::cout << "Part Two: " << PartTwo(File) << std::endl;
+}
+
+unsigned int PartOne(const StringVector & Lines)
+{
 	unsigned int Doubles = 0;
 	unsigned int Triples = 0;
 
-	for (std::string Line : File)
+	for (const std::string & Line : Lines)
 	{
-		std::unordered_map<std::string::value_type, unsigned int> Appearances;
-		std::for_each(Line.begin(), Line.end(), [&](const std::string::value_type & character) { ++Appearances[character]; });
+		std::unordered_map<std::string::value_type, unsigned int> CharacterCount;
+		for (const std::string::value_type & Character : Line)
+			++CharacterCount[Character];
 
 		bool HasDouble = false;
 		bool HasTripple = false;
 
-		for (std::pair<std::string::value_type, unsigned int> Count : Appearances)
+		for (std::pair<std::string::value_type, unsigned int> Count : CharacterCount)
 		{
 			if (Count.second == 2)
 				HasDouble = true;
@@ -36,5 +46,30 @@ int main()
 			++Triples;
 	}
 
-	std::cout << "Part One: " << (Doubles * Triples) << std::endl;
+	return (Doubles * Triples);
+}
+
+std::string PartTwo(const StringVector & Lines)
+{
+	for (auto Line = Lines.begin(); Line != Lines.end(); ++Line)
+	{
+		for (auto CompareLine = (Line + 1); CompareLine != Lines.end(); ++CompareLine)
+		{
+			auto FirstMismatch = std::mismatch(Line->begin(), Line->end(), CompareLine->begin());
+
+			// Both are equal, should never occur or by the puzzle definition not a solution.
+			if (FirstMismatch.first == Line->end() || FirstMismatch.second == CompareLine->end())
+				continue;
+
+			auto SecondMismatchCheck = std::mismatch(FirstMismatch.first + 1, Line->end(), FirstMismatch.second + 1);
+
+			// After the first Mismatch, rest of both is the same, therefore those are the IDs we are looking for
+			if (SecondMismatchCheck.first == Line->end() && SecondMismatchCheck.second == CompareLine->end())
+			{
+				return std::string(*Line).erase(std::distance(Line->begin(), FirstMismatch.first), 1);
+			}
+		}
+	}
+
+	return "No Match found!";
 }
--- 2018/Day02/pch.h
+++ 2018/Day02/pch.h
@@ -1,6 +1,7 @@
 #ifndef PCH_H
 #define PCH_H
 
+#include <iterator>
 #include <iostream>
 #include <unordered_map>
 
