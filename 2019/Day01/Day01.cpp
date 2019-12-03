8cc8497f4a1bbd13a8159c771c5c8643b59a50f2
Julian Löhr <julian.loehr@outlook.com>
2019-12-03 22:35:21 +0100

    Day 01 part two

--- 2019/Day01/Day01.cpp
+++ 2019/Day01/Day01.cpp
@@ -7,7 +7,22 @@ int main()
 {
 	std::vector<uint64_t> File = GetValues<uint64_t>("Input.txt");
 
-	uint64_t Fuel = std::accumulate(File.cbegin(), File.cend(), static_cast<uint64_t>(0u), [](uint64_t sum, uint64_t value) -> uint64_t { return sum + ((value / 3) - 2); });
+	uint64_t FuelPartOne = std::accumulate(File.cbegin(), File.cend(), static_cast<uint64_t>(0u), [](uint64_t sum, uint64_t value) -> uint64_t { return sum + ((value / 3) - 2); });
+	uint64_t FuelPartTwo = std::accumulate(
+		File.cbegin(), 
+		File.cend(), 
+		static_cast<uint64_t>(0u), 
+		[](uint64_t sum, uint64_t value) -> uint64_t 
+		{ 
+			while (value > 0)
+			{
+				value = std::max<uint64_t>((value / 3), 2) - 2;
+				sum += value;
+			}
 
-	std::cout << "Required fuel: " << Fuel << std::endl;
+			return sum;
+		});
+
+	std::cout << "Part One: " << FuelPartOne << std::endl;
+	std::cout << "Part Two: " << FuelPartTwo << std::endl;
 }
--- 2019/Day01/pch.h
+++ 2019/Day01/pch.h
@@ -1,6 +1,7 @@
 #ifndef PCH_H
 #define PCH_H
 
+#include <algorithm>
 #include <numeric>
 
 #include "../../Common/Common.h"
