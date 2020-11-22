b9dc682b66371eb709460c8297218107e29016ab
Julian Löhr <julian.loehr@outlook.com>
2020-11-22 14:42:45 +0100

    Day 08 part two

--- 2018/Day08/Day08.cpp
+++ 2018/Day08/Day08.cpp
@@ -3,38 +3,64 @@
 
 #include "pch.h"
 
+typedef std::vector<uint64_t> IntVector;
+typedef std::shared_ptr<IntVector> IntVectorPtr;
+
+struct Node
+{
+	uint64_t AmountChildNodes;
+	uint64_t AmountMetaData;
+	IntVectorPtr ChildNodesValues;
+};
+
 int main()
 {
 	const StringVector File = GetFileLineParts("Input.txt")[0];
 
-	uint64_t CheckSum = 0;
-	std::stack<std::pair<uint64_t, uint64_t>> NodeStack;
+	uint64_t CheckSumPartOne = 0;
+	std::stack<Node> NodeStack;
 
 	auto It = File.cbegin();
 	uint64_t AmountChildNodes = 1;
 	uint64_t AmountMetaData = 0;
+	IntVectorPtr ChildNodesValues = std::make_shared<IntVector>();
 
 	while(It != File.cend())
 	{
 		if (AmountChildNodes > 0)
 		{
-			NodeStack.push({ AmountChildNodes, AmountMetaData });
+			NodeStack.push({ AmountChildNodes, AmountMetaData, ChildNodesValues });
 
 			AmountChildNodes = std::atoi(It++->c_str());
-			AmountMetaData = std::atoi(It++->c_str());
+			AmountMetaData = std::atoi(It++->c_str()); 
+			ChildNodesValues = (AmountChildNodes > 0) ? std::make_shared<IntVector>() : nullptr;
 		}
 		else
 		{
+			uint64_t MetaDataSum = 0;
+			uint64_t NodeValue = 0;
 			for (; AmountMetaData > 0; AmountMetaData--)
-				CheckSum += std::atoi(It++->c_str());
+			{
+				uint64_t MetaDataValue = std::atoi(It++->c_str());
+				MetaDataSum += MetaDataValue;
+				if (ChildNodesValues && ((MetaDataValue - 1) < ChildNodesValues->size()))
+					NodeValue += ChildNodesValues->operator[](MetaDataValue - 1);
+			}
 
-			AmountChildNodes = NodeStack.top().first;
-			AmountMetaData = NodeStack.top().second;
+			NodeValue = ChildNodesValues ? NodeValue : MetaDataSum;
+
+			AmountChildNodes = NodeStack.top().AmountChildNodes;
+			AmountMetaData = NodeStack.top().AmountMetaData;
+			ChildNodesValues = NodeStack.top().ChildNodesValues;
 			NodeStack.pop();
+
+			CheckSumPartOne += MetaDataSum;
+			ChildNodesValues->push_back(NodeValue);
 
 			AmountChildNodes--;
 		}
 	}
 
-	std::cout << "Check Sum: " << CheckSum << std::endl;
+	std::cout << "Part One: " << CheckSumPartOne << std::endl;
+	std::cout << "Part Two: " << ChildNodesValues->operator[](0) << std::endl;
 }
