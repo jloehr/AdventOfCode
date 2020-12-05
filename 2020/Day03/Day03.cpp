979d3a8ee71c5c9e922bc1ad7b844f0a40727a99
Julian Löhr <julian.loehr@outlook.com>
2020-12-05 16:51:49 +0100

    Day 03 part two

--- 2020/Day03/Day03.cpp
+++ 2020/Day03/Day03.cpp
@@ -1,26 +1,49 @@
 // Day03.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <numeric>
+
 #include "../../Common/Common.h"
+
+constexpr size_t PartOneIndex = 1;
+
+typedef std::pair<size_t, size_t> Coordinate;
+
+size_t Traverse(const StringVector& Map, const Coordinate& Velocity);
 
 int main()
 {
-	StringVector Lines = GetFileLines("Input.txt");
-	std::pair<size_t, size_t> CurrentPosition = { 0, 0 };
-	std::pair<size_t, size_t> Velocity = { 3, 1 };
+	const StringVector Lines = GetFileLines("Input.txt");
+	const std::vector<Coordinate> Slopes = {
+		{1,1},
+		{3,1},
+		{5,1},
+		{7,1},
+		{1,2},
+	};
+
+	std::vector<size_t> TreeCounts;
+	std::transform(Slopes.cbegin(), Slopes.cend(), std::back_inserter(TreeCounts), [&](const Coordinate& Velocity) -> size_t { return Traverse(Lines, Velocity); });
+
+	std::cout << "Part One: " << TreeCounts[PartOneIndex] << std::endl;
+	std::cout << "Part Two: " << std::accumulate(TreeCounts.cbegin(), TreeCounts.cend(), static_cast<size_t>(1), std::multiplies<size_t>()) << std::endl;
+}
+
+size_t Traverse(const StringVector& Map, const Coordinate& Velocity)
+{
+	Coordinate CurrentPosition = { 0, 0 };
 	size_t TreeCount = 0;
 
-	while (CurrentPosition.second < Lines.size())
+	while (CurrentPosition.second < Map.size())
 	{
-		CurrentPosition.first %= Lines[CurrentPosition.second].size();
+		CurrentPosition.first %= Map[CurrentPosition.second].size();
 
-		if (Lines[CurrentPosition.second][CurrentPosition.first] == '#')
+		if (Map[CurrentPosition.second][CurrentPosition.first] == '#')
 			TreeCount++;
 
 		CurrentPosition.first += Velocity.first;
 		CurrentPosition.second += Velocity.second;
 	}
 
-	std::cout << "Part One: " << TreeCount << std::endl;
+	return TreeCount;
 }
-
