2f87f500da1826d0ea9f9becdc39a2ac6735a0b1
Julian Löhr <julian.loehr@outlook.com>
2020-12-05 12:39:15 +0100

    Day 02 part two

--- 2020/Day02/Day02.cpp
+++ 2020/Day02/Day02.cpp
@@ -10,7 +10,8 @@ int main()
 	std::regex LineMatching("(\\d+)-(\\d+)\\s(\\S):\\s(\\S+)", std::regex_constants::optimize);
 
 	StringVector Lines = GetFileLines("Input.txt");
-	size_t ValidPasswords = 0;
+	size_t PartOne = 0;
+	size_t PartTwo = 0;
 
 	for(const std::string & Line : Lines)
 	{
@@ -21,14 +22,18 @@ int main()
 			return 1;
 		}
 
-		size_t CharacterCount = std::count(Matches[4].first, Matches[4].second, Matches[3]);
-		size_t MinCharacterCount = std::stoull(Matches[1]);
-		size_t MaxCharacterCount = std::stoull(Matches[2]);
+		size_t Lower = std::stoull(Matches[1]);
+		size_t Higher = std::stoull(Matches[2]);
 
-		if ((CharacterCount >= MinCharacterCount) && (CharacterCount <= MaxCharacterCount))
-			ValidPasswords++;
+		size_t CharacterCount = std::count(Matches[4].first, Matches[4].second, Matches[3]);
+		if ((CharacterCount >= Lower) && (CharacterCount <= Higher))
+			PartOne++;
+
+		if ((*(Matches[4].first + (Lower - 1)) == Matches[3]) != (*(Matches[4].first + (Higher - 1)) == Matches[3]))
+			PartTwo++;
 	}
 
-	std::cout << "Part One: " << ValidPasswords << std::endl;
+	std::cout << "Part One: " << PartOne << std::endl;
+	std::cout << "Part Two: " << PartTwo << std::endl;
 
 }
