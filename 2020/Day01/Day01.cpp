eb6801a9585faf35369d32351cf40b4e391a9620
Julian Löhr <julian.loehr@outlook.com>
2020-12-05 12:04:42 +0100

    Day 01 part two

--- 2020/Day01/Day01.cpp
+++ 2020/Day01/Day01.cpp
@@ -6,10 +6,9 @@
 int main()
 {
 	std::vector<uint16_t> Values = GetValues<uint16_t>("Input.txt");
-
 	std::sort(Values.begin(), Values.end());
-	auto LowValue = Values.begin();
-	auto HighValue = Values.end() - 1;
+	auto LowValue = Values.cbegin();
+	auto HighValue = Values.cend() - 1;
 
 	while(HighValue != LowValue)
 	{
@@ -24,7 +23,35 @@ int main()
 	}
 
 	if (HighValue == LowValue)
-		std::cout << "No Solution found!" << std::endl;
+		std::cout << "Part One: No Solution found!" << std::endl;
 	else
 		std::cout << "Part One: " << (*HighValue * *LowValue) << std::endl;
+
+	auto MidValue = LowValue;
+	for(;HighValue != Values.cbegin(); HighValue--)
+	{
+		while (((*HighValue + *MidValue) < 2020) && (HighValue > MidValue))
+			MidValue++;
+
+		while (((*HighValue + *MidValue) > 2020) && (MidValue > Values.cbegin()))
+			MidValue--;
+
+		if ((HighValue == MidValue) || (MidValue == Values.cbegin()))	
+			continue;
+
+		for(auto MovingMidValue = MidValue; MovingMidValue != Values.cbegin(); MovingMidValue--)
+		{
+			LowValue = Values.cbegin();
+			while ((LowValue != MovingMidValue) && ((*HighValue + *MovingMidValue + *LowValue) < 2020))
+				LowValue++;
+
+			if ((*HighValue + *MovingMidValue + *LowValue) == 2020)
+			{
+				std::cout << "Part Two: " << (*HighValue * *MovingMidValue  * *LowValue) << std::endl;
+				return 0;
+			}
+		}
+	}
+
+	std::cout << "Part Two: No Solution found!" << std::endl;
 }
