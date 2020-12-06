9dae03c6bc0ded685fff4bfb44e1d3eb374d5a9b
Julian Löhr <julian.loehr@outlook.com>
2020-12-06 18:55:55 +0100

    Day 05 part two

--- 2020/Day05/Day05.cpp
+++ 2020/Day05/Day05.cpp
@@ -1,12 +1,16 @@
 // Day05.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <set>
+
 #include "../../Common/Common.h"
 
 int main()
 {
 	StringVector Lines = GetFileLines("Input.txt");
 	size_t HighestSeatID = 0;
+	size_t YourSeatID = 0;
+	std::set<size_t> TakenSeats;
 
 	for (const std::string& Line : Lines)
 	{
@@ -22,9 +26,24 @@ int main()
 			}
 		}
 
+		TakenSeats.insert(SeatID);
+
 		if (SeatID > HighestSeatID)
 			HighestSeatID = SeatID;
 	}
 
+	for (auto It = TakenSeats.cbegin(); It != TakenSeats.cend();)
+	{
+		size_t SeatID = *It;
+		size_t NextSeatID = *(++It);
+
+		if (SeatID == (NextSeatID - 2))
+		{	
+			YourSeatID = SeatID + 1;
+			break;
+		}
+	}
+
 	std::cout << "Part One:" << HighestSeatID << std::endl;
+	std::cout << "Part To:" << YourSeatID << std::endl;
 }
