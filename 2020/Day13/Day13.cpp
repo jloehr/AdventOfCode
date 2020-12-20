600bb124dcff33636555786c817e3a1e9c6abff4
Julian Löhr <julian.loehr@outlook.com>
2020-12-20 23:06:53 +0100

    Day 13 part two

--- 2020/Day13/Day13.cpp
+++ 2020/Day13/Day13.cpp
@@ -3,30 +3,56 @@
 
 #include <regex>
 #include <algorithm>
+#include <numeric>
 
 #include "../../Common/Common.h"
 
+struct Bus
+{
+	size_t BusID;
+	size_t NextDepartureIn;
+	size_t Offset;
+};
+
 int main()
 {
-	const std::regex BusMatching("(\\d+)");
+	const std::regex BusMatching("((\\d+)|x)");
 	StringVector Lines = GetFileLines("Input.txt");
 
 	size_t Timestamp = std::stoull(Lines[0]);
-	std::vector<std::pair<size_t, size_t>> Buses;
-
-	std::transform(
+	std::vector<Bus> Buses = std::accumulate(
 		std::sregex_iterator(std::cbegin(Lines[1]), std::cend(Lines[1]), BusMatching), 
 		std::sregex_iterator(), 
-		std::back_inserter(Buses), 
-		[=](const std::smatch& Match)->std::pair<size_t, size_t> 
+		std::vector<Bus>(),
+		[=](std::vector<Bus> Buses, const std::smatch& Match)->std::vector<Bus>
 		{ 
-			size_t BusID = std::stoull(Match.str());
-			size_t NextDepartureIn = (BusID - (Timestamp % BusID));
-			return { BusID, NextDepartureIn };
+			static size_t Offset = 0;
+
+			if (Match.str() != "x")
+			{
+				size_t BusID = std::stoull(Match.str());
+				size_t NextDepartureIn = (BusID - (Timestamp % BusID));
+				Buses.push_back(Bus{ BusID, NextDepartureIn, Offset });
+			}
+
+			Offset++;
+			return std::move(Buses);
 		}
 	);
 
-	auto NextBus = std::min_element(std::cbegin(Buses), std::cend(Buses), [](const std::pair<size_t, size_t>& A, const std::pair<size_t, size_t>& B)->bool { return A.second < B.second; });
+	auto NextBus = std::min_element(std::cbegin(Buses), std::cend(Buses), [](const Bus& A, const Bus& B)->bool { return A.NextDepartureIn < B.NextDepartureIn; });
 
-	std::cout << "Part One: " << (NextBus->first * NextBus->second) << std::endl;
+	std::cout << "Part One: " << (NextBus->BusID * NextBus->NextDepartureIn) << std::endl;
+
+	std::sort(std::begin(Buses), std::end(Buses), [](const Bus& A, const Bus& B)->bool { return A.BusID > B.BusID; });
+	Timestamp = std::cbegin(Buses)->BusID - std::cbegin(Buses)->Offset;
+	size_t TimestampSteps = std::cbegin(Buses)->BusID;
+
+	for (auto Bus = std::next(std::cbegin(Buses)); Bus != std::cend(Buses); Bus++)
+	{
+		for (; ((Timestamp + Bus->Offset) % Bus->BusID) != 0; Timestamp += TimestampSteps);
+		TimestampSteps *= Bus->BusID;
+	}
+
+	std::cout << "Part Two: " << Timestamp << std::endl;
 }
